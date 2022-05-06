d7defe6453a47a68439727de0e793e2337593e3d
Gauri <gaurishinde2518@gmail.com>
2022-05-07 01:32:45 +0530

    commiting iframe class

---
+++ src/test/java/module-info.java
@@ -0,0 +1,9 @@
+module SampleProgrammes {
+    requires selenium.chrome.driver;
+    requires io.github.bonigarcia.webdrivermanager;
+    requires selenium.api;
+    requires org.testng;
+    requires selenium.support;
+    requires selenium.firefox.driver;
+    requires java.desktop;
+}
